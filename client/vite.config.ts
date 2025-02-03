5fac2dcf5dc0ada707d9786ff0e148f4e4d9bc5b
Jamie Wilson <howemanning@gmail.com>
2025-02-04 10:38:05 +1300

    Deploy to github

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/brbsavingthat/", // 🔹 Change this to match your GitHub repo name
+});
