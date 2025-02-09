1c90058af542c03f4db42dcbb4fbd01b51241a2e
Jamie Wilson <howemanning@gmail.com>
2025-02-09 14:24:17 +1300

    Remove terser

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: "terser", // ✅ Minifies JavaScript
-  },
-  server: {
-    headers: {
-      "Cache-Control": "public, max-age=31536000, immutable",
-    },
-  },
+
   base: "/be_write_back/", // 🔹 Change this to match your GitHub repo name
 });
