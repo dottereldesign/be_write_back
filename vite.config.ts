32791bbe06b6ccbe0d3c8cda38c3f13b018c6604
Jamie Wilson <howemanning@gmail.com>
2025-05-11 19:46:50 +1200

    Add icons for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -25,14 +25,14 @@ export default defineConfig({
         start_url: "/be_write_back/", // ⚡ MATCH your base!
         icons: [
           {
-            src: "vite.svg",
+            src: "icon-192.png",
             sizes: "192x192",
-            type: "image/svg+xml",
+            type: "image/png",
           },
           {
-            src: "vite.svg",
+            src: "icon-512.png",
             sizes: "512x512",
-            type: "image/svg+xml",
+            type: "image/png",
           },
         ],
       },
